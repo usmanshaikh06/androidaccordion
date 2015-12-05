3e19a82fb1ba5f385604c6b8d4025c8c95e4e6c7
Usman Shaikh <usman_segi@yahoo.com>
2015-12-05 20:35:41 +0500

    Initial Commit new library project with empty accordion class

---
+++ library/src/main/java/com/github/usmanshaikh06/androidaccordion/Accordion.java
@@ -0,0 +1,22 @@
+package com.github.usmanshaikh06.androidaccordion;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Ahsan on 05-Dec-15.
+ */
+public class Accordion extends LinearLayout {
+    public Accordion(Context context) {
+        super(context);
+    }
+
+    public Accordion(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Accordion(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context,attrs,defStyleAttr);
+    }
+}
